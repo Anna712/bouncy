ac3f5aad932f8e81e78883620c0e21c1e183a060
Anny Dev <nya712@gmail.com>
2018-09-01 16:49:54 +0300

    header

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,12 @@
 $(function() {
 
-	// Custom JS
-
+	$('#nav-toggle').on('click', function() {
+		$(this).toggleClass('active');
+		$('nav ul').slideToggle (300, function(){
+			if ($(this).css('display') === 'none') {
+				$(this).removeAttr('style');
+			}
+		});
+	});
+	
 });
